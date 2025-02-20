21691a331021b7572771e0e35abdc454d6fffb51
Georgii Gvinepadze <georgii.gvinepadze@dbeaver.com>
2025-02-20 16:06:32 +0400

    dbeaver/pro#4212 rename plugins

--- plugins/org.jkiss.dbeaver.ai.copilot/src/org/jkiss/dbeaver/ai/copilot/model/request/CopilotRequestService.java
+++ plugins/org.jkiss.dbeaver.ai.copilot/src/org/jkiss/dbeaver/ai/copilot/model/request/CopilotRequestService.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.copilot.model.request;
+package org.jkiss.dbeaver.ai.copilot.model.request;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
